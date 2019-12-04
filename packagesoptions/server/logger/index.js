6faf387263c09b6786400ba32124d318eee2263e
Kevin Jones <kevin.jones@Carvana.com>
2019-12-04 19:32:49 +0000

    Merged PR 63992: Catch for blank/null splunk token

--- packagesoptions/server/logger/index.js
+++ packagesoptions/server/logger/index.js
@@ -4,7 +4,7 @@ import config from '../config/config';
 const {CARVANA_APP_SPLUNK_TOKEN, CARVANA_APP_SPLUNK_URL} = config;
 
 const serverSystemName = 'SEO_ReviewsService';
-const splunkLogger = new Logger({
+const splunkLogger = (typeof(CARVANA_APP_SPLUNK_TOKEN) ==='string' && CARVANA_APP_SPLUNK_TOKEN.length >0) && new Logger({
   token: `${CARVANA_APP_SPLUNK_TOKEN}`,
   url: `${CARVANA_APP_SPLUNK_URL}`,
   port: 443
@@ -12,15 +12,18 @@ const splunkLogger = new Logger({
 
 export const sendSplunkLog = logDetail => {
   const {logMessage, status, responseBody, query} = logDetail;
-  splunkLogger.send({
-    message: {
-      msg: `${serverSystemName} - ${logMessage}`,
-      serverName: serverSystemName,
-      status,
-      responseBody,
-      query: JSON.stringify(query)
-    }
-  });
+  
+  if(splunkLogger !== false){
+    splunkLogger.send({
+      message: {
+        msg: `${serverSystemName} - ${logMessage}`,
+        serverName: serverSystemName,
+        status,
+        responseBody,
+        query: JSON.stringify(query)
+      }
+    });
+  }
 }
 
 
