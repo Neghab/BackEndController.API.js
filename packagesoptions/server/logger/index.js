ae9ed6fde5d2fcb9f6aec430585cafafc6ed33cf
Kevin Jones <kevin.jones@carvana.com>
2019-12-23 15:47:04 -0700

    updated environments with new keys/secrets; updated API to support ENV vars for cosmos;

--- packagesoptions/server/logger/index.js
+++ packagesoptions/server/logger/index.js
@@ -1,12 +1,12 @@
 import { Logger } from 'splunk-logging';
 import config from '../config/config';
 
-const {CARVANA_APP_SPLUNK_TOKEN, CARVANA_APP_SPLUNK_URL} = config;
+const {CVNA_APP_SPLUNK_TOKEN, CVNA_APP_SPLUNK_URL} = config;
 
 const serverSystemName = 'AlgoContent_PackagesOptionsAPI';
-export const splunkLogger = (typeof(CARVANA_APP_SPLUNK_TOKEN) ==='string' && CARVANA_APP_SPLUNK_TOKEN.length >0) && new Logger({
-  token: `${CARVANA_APP_SPLUNK_TOKEN}`,
-  url: `${CARVANA_APP_SPLUNK_URL}`,
+export const splunkLogger = (typeof(CVNA_APP_SPLUNK_TOKEN) ==='string' && CVNA_APP_SPLUNK_TOKEN.length >0) && new Logger({
+  token: `${CVNA_APP_SPLUNK_TOKEN}`,
+  url: `${CVNA_APP_SPLUNK_URL}`,
   port: 443
 });
 
